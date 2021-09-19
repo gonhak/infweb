ca5c5e38b9d2677b52fcdee44928a6175ce41c30
Filip Natanek <natanek291@gmail.com>
2021-09-19 18:53:42 +0200

    made the prototype of the main page and made the new subpages for each one courses

--- main.js
+++ main.js
@@ -4,30 +4,44 @@ const navBtnIcon = document.querySelector('.nav__colapse__icon');
 const loginPage = document.querySelector('.loginScreen');
 const input = document.querySelector('.loginScreen__box__input');
 const loginBtn = document.querySelector('.loginScreen__box__button');
-const password = 'fnatanek';
+const passwords = ["fnatanek", "mkoziel", "rrady"];
+let password;
 
 const checkingPass = () => {
-    
-    if(input.value === password){
+    passwords.forEach(el => {
+        if (input.value === el) {
+            password = el;
+        }
+    });
+
+    if (input.value === password && input.value !== "") {
         loginPage.style.opacity = 0;
         loginPage.style.zIndex = '-1';
-    }else if(input.value !== password){
+    } else if (input.value !== password || input.value == "") {
         input.value = '';
         input.placeholder = "Spróbuj ponownie...."
-    }
+    };
 
+    password = "";
 };
 
-navBtn.addEventListener('click',() => {
+const checkingPassInput = (event) => {
+    if(event.keyCode == 13){
+        checkingPass();
+    };
+};
+
+navBtn.addEventListener('click', () => {
     navBtn.classList.toggle('active');
 
-    if(navBtn.classList.contains('active')){
+    if (navBtn.classList.contains('active')) {
         nav.style.width = 250 + 'px';
-        navBtnIcon.style.transform = 'rotateY(-180deg)'
-    }else {
+        navBtnIcon.style.transform = 'rotateY(-180deg)';
+    } else {
         nav.style.width = 100 + 'px';
-        navBtnIcon.style.transform = 'rotateY(0deg)'
+        navBtnIcon.style.transform = 'rotateY(0deg)';
     };
 });
 
 loginBtn.addEventListener('click', checkingPass);
+input.addEventListener('keydown', checkingPassInput);
